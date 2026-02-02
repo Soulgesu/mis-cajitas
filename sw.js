b5b61b04707f3b8d4de0d6959ffc5b427f0f0b6a
Soulgesu <lizarragalm64@gmail.com>
2026-02-02 18:21:55 -0500

    update to v2

--- sw.js
+++ sw.js
@@ -1,25 +1,23 @@
-const CACHE_NAME = 'cajitas-v1';
+const CACHE_NAME = 'cajitas-v2';
 const ASSETS = ['./', './index.html', './style.css', './script.js', './manifest.json'];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting(); 
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
     );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
-});
-
-// Limpieza de caches antiguos
 self.addEventListener('activate', (event) => {
     event.waitUntil(
         caches.keys().then((keys) => {
             return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
         })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
     );
 });
